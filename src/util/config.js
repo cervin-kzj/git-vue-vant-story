c77026c3f6926781f49c7e79c4b7c97b7e807848
kzj <1324404675@qq.com>
2020-09-16 06:57:52 +0800

    修改接口配置文件

--- src/util/config.js
+++ src/util/config.js
@@ -1,7 +1,7 @@
 const { config } = require("shelljs");
 
-const appid = "330218"; //应用id
-const sign = "7ffac93b348c4d2eafab14527aac3442"; //秘钥
+const appid = "171022"; //应用id
+const sign = "dcece2aea56a4909973456bbba06ff1b"; //秘钥
 
 export const conf={
     appid,
